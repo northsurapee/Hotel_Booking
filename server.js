224f6797a5ab8c7f34f99163a280504ee0dffe29
northsurapee <northsurapee@gmail.com>
2024-04-12 17:15:15 +0700

    build: set up nodejs server and connect to mongoDB

---
+++ server.js
@@ -0,0 +1,96 @@
+const express = require("express");
+const dotenv = require("dotenv");
+const cookieParser = require("cookie-parser");
+const connectDB = require("./config/db");
+const cors = require("cors");
+const mongoSanitize = require("express-mongo-sanitize");
+const helmet = require("helmet");
+const rateLimit = require("express-rate-limit");
+const hpp = require("hpp");
+const { xss } = require("express-xss-sanitizer");
+const swaggerJsDoc = require("swagger-jsdoc");
+const swaggerUI = require("swagger-ui-express");
+
+// Load env vars
+dotenv.config({ path: "./config/config.env" });
+
+// Connect to database
+connectDB();
+
+// Create express app
+const app = express();
+
+// Enable CORS for all routes
+app.use(cors());
+
+// Body parser
+app.use(express.json());
+
+//Sanitize data
+app.use(mongoSanitize());
+
+//Set security headers
+app.use(helmet());
+
+//Prevent XSS attacks
+app.use(xss());
+
+//Rate Limiting
+const limiter = rateLimit({
+  windowsMs: 10 * 60 * 1000, //10 mins
+  max: 100,
+});
+app.use(limiter);
+
+//Prevent http param pollutions
+app.use(hpp());
+
+// Cookie parser
+app.use(cookieParser());
+
+// // Forward to Routes middleware (if endpoint matched)
+// const hospitals = require("./routes/hospitals");
+// const auth = require("./routes/auth");
+// const appointments = require("./routes/appointments");
+
+// app.use("/api/v1/hospitals", hospitals);
+// app.use("/api/v1/auth", auth);
+// app.use("/api/v1/appointments", appointments);
+
+const swaggerOptions = {
+  swaggerDefinition: {
+    openapi: "3.0.0",
+    info: {
+      title: "Library API",
+      version: "1.0.0",
+      description: "A simple Express VacQ API",
+    },
+    servers: [
+      {
+        url: "http://localhost:3000/api/v1",
+      },
+    ],
+  },
+  apis: ["./routes/*.js"],
+};
+const swaggerDocs = swaggerJsDoc(swaggerOptions);
+app.use("/api-docs", swaggerUI.serve, swaggerUI.setup(swaggerDocs));
+
+// Listen
+const PORT = process.env.PORT || 3000;
+const server = app.listen(
+  PORT,
+  console.log(
+    "Server running in ",
+    process.env.NODE_ENV,
+    " mode on port ",
+    PORT
+  )
+);
+
+// Handle unhandles promise rejections
+process.on("unhandledRejection", (err, promise) => {
+  console.log(`Error: ${err.message}`);
+  // Close sever & exit process
+  server.close(() => process.exit(1));
+});
